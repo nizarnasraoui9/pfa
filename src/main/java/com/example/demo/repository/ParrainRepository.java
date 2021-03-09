a714c7f25b3ce1d0afcc8ebcc8d1badd5ab31e33
nizarnasraoui9 <nizarnasraoui9@gmail.com>
2021-03-09 01:48:48 +0100

    updated parrain

--- src/main/java/com/example/demo/repository/ParrainRepository.java
+++ src/main/java/com/example/demo/repository/ParrainRepository.java
@@ -1,12 +1,16 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Parrain;
+import com.example.demo.model.Personne;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.ArrayList;
 
 @RepositoryRestResource(collectionResourceRel = "parrain",path="parrain")
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ParrainRepository extends JpaRepository<Parrain,Long> {
     Parrain findById(long id);
+    Parrain findByNomAndPrenom(String nom, String prenom);
 }
